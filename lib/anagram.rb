24332a6ee10863de982d8be3907c25b99eace29b
Asif Kamal <asif_kamal@hotmail.com>
2020-01-17 14:24:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,20 @@
 class Anagram
   
-  attr_accessor :email_addresses
+  attr_accessor :word
   
   
   def initialize(word)
+    @word = word
   end
+  
+  
+  def match(words)
+    words.each do |wrd|
+      if wrd.split("").sort == @word.split("").sort
+        wrd
+      end
+    end
+  end
+      
     
 end
